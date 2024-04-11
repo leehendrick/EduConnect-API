bec71b916455056e99dec663c5258fde88c4cac1
leehendrick <imhendrick16@gmail.com>
2024-04-11 14:37:49 +0100

    add create user methos

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,8 +1,25 @@
 import {FastifyReply, FastifyRequest} from "fastify";
+import * as repl from "repl";
+import {createUser} from "./user.service";
+import {CreateUserInput} from "./user.schemas";
 
 export async function registerUser(
-        request: FastifyRequest,
+        request: FastifyRequest<{
+            Body: CreateUserInput;
+        }>,
         reply: FastifyReply
     ){
 
+    const body = request.body
+
+    try{
+
+        const user = await createUser(body);
+
+        return reply.code(201).send(user);
+
+    }catch (e){
+        console.log(e);
+        return reply.code(500).send(e)
+    }
 }
