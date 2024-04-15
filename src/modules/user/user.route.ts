e855299cb9819d3eaf50d2f3455f25a6db0b4bff
leehendrick <imhendrick16@gmail.com>
2024-04-15 14:28:36 +0100

    update file

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,8 +1,11 @@
 import { FastifyInstance } from "fastify";
 import {registerUser} from "./user.controller";
+import { $ref } from "./user.schemas";
 
 async function userRoute(server: FastifyInstance){
-    server.post('/create', registerUser)
+    server.post(
+        '/create',
+        registerUser)
 }
 
 export default userRoute
