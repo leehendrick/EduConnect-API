8c9ea271881d7779c8f8f4002555e6e3cc76bcd0
leehendrick <imhendrick16@gmail.com>
2024-04-15 14:38:03 +0100

    set the response

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,10 +1,19 @@
 import { FastifyInstance } from "fastify";
 import {registerUser} from "./user.controller";
 import { $ref } from "./user.schemas";
+import {createUser} from "./user.service";
 
 async function userRoute(server: FastifyInstance){
     server.post(
         '/create',
+        {
+            schema: {
+                body: $ref('createUserSchema'),
+                response: {
+                    201: $ref('createUserResponseSchema'),
+                }
+            }
+        },
         registerUser)
 }
 
