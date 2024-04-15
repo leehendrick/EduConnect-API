d64ef00919c0a28a76bf2d7a2111a2b0360e359e
leehendrick <imhendrick16@gmail.com>
2024-04-15 19:43:51 +0100

    add senha in the login schema

--- src/modules/user/user.schemas.ts
+++ src/modules/user/user.schemas.ts
@@ -52,6 +52,9 @@ const loginSchema = z.object({
     email: z.string({
         required_error: "Email is required",
         invalid_type_error: "Email must be a string",
+    }),
+    senha: z.string({
+        required_error: "Email is required",
     })
 })
 
